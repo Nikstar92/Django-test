8aa6403150141d02c1514fd7879e7bc196b1431c
Nikita <Nikita.yavkin92@gmail.com>
2023-02-09 16:05:38 +0300

    start_project_books

---
+++ books_project/books/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h2>Главная</h2>")
+
+
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+
+
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
---
+++ books_project/books_project/urls.py
@@ -0,0 +1,28 @@
+"""books_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from books import views
+
+
+
+urlpatterns = {
+    re_path(r'^about/contact/', views.contact),
+    re_path(r'^about', views.about),
+    path('', views.index),
+    path('admin/', admin.site.urls),
+
+}
